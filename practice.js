0972257e4ecacde556561ee94f415d183b40eb4f
Sara-Kuddah <sara-kuddah@hotmail.com>
2020-01-22 23:55:25 +0300

    (Array Practice 5 + 6) are done.

--- practice.js
+++ practice.js
@@ -14,7 +14,7 @@ getMeTheLastElemInAnArray(strs2);
 getMeTheLastElemInAnArray(strs3);
 
 
-//practice 2.
+//practice 4. Starting with an empty array called rainbowColors:
 const rainbowColors =[];
 rainbowColors.push("orange");
 rainbowColors.unshift("red");
@@ -26,3 +26,18 @@ console.log(rainbowColors.length);
 console.log(rainbowColors[1]);
 console.log(rainbowColors[rainbowColors.length-1]);
 console.log(rainbowColors.indexOf("red"));
+console.log(rainbowColors);
+//practice 5 Find out the difference between .slice and .splice
+
+const twoColors=rainbowColors.slice(1,3);
+console.log(twoColors);
+
+
+//practice 6 Work with arrays of arrays
+const arrOfArrs = [["inner array first item", "inner array second item"],
+ ["first", "second", "third"]];
+//Access "inner array first item" and print it out"
+//Print "third" by using a dynamic index
+console.log(arrOfArrs[0][0]); 
+console.log(arrOfArrs[1][2]);
+
